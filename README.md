947b8cb1b0e8441ce052fddf77d7690b3394dd5d
Jakob A <ja@impera.se>
2022-03-01 08:48:38 +0100

    Added etch a sketch

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # top-etch-a-sketch
+
+# This is a project regarding Etch a Sketch on The Odin Project. Ask me anything
---
+++ script.js
@@ -0,0 +1,33 @@
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
+// Creates a default grid sized 16x16
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
+}
+
+// Takes (rows, columns) input and makes a grid
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+      let cell = document.createElement("div");
+      cell.innerText = (c + 1);
+      container.appendChild(cell).className = "grid-item";
+    };
+  };
+  
+  makeRows(16, 16);
+
+// Creates columns
+function makeColumns(cellNum) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        };
+
+    };
+};

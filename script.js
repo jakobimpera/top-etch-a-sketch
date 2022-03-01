5dc1d2a8ae3ba5b76692a8aee43f909c4faed653
Jakob A <ja@impera.se>
2022-03-01 13:24:28 +0100

    Updated app

--- script.js
+++ script.js
@@ -2,32 +2,40 @@ const container = document.getElementById("container");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
 
-// Creates a default grid sized 16x16
+// Skapar en grid 16x16
 function defaultGrid() {
     makeRows(16);
     makeColumns(16);
+    console.log('hej');    
+    document.getElementsByClassName('grid-item').addEventListener('mouseover', function() {
+         document.getElementsClassName(("grid-item").classList.add("mouse-hover"));
+        });
+            
+        document.getElementsClassName(("grid-item").addEventListener("mouseout"), function() {
+        document.getElementsClassName(("grid-item").classList.remove("mouse-hover"));
+       });
 }
 
-// Takes (rows, columns) input and makes a grid
+// Tar rows & Cols och skapar en grid
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
       let cell = document.createElement("div");
-      cell.innerText = (c + 1);
+      //cell.innerText = (c + 1);
       container.appendChild(cell).className = "grid-item";
     };
   };
-  
   makeRows(16, 16);
 
-// Creates columns
+// Skapar kolumner
 function makeColumns(cellNum) {
     for (i = 0; i < rows.length; i++) {
         for (j = 0; j < cellNum; j++) {
             let newCell = document.createElement("div");
             rows[j].appendChild(newCell).className = "cell";
         };
-
-    };
+    }; //Jag vill ändra färg på "grid-item?". Hur gör jag det enklast? Ändra class? Göra en background-color change?
+    
 };
+

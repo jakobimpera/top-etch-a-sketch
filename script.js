53bc36cbe30ef69b75aa5b5692b780fd2da081b5
Jakob A <ja@impera.se>
2022-03-01 21:30:30 +0100

    updated files to new version

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 const container = document.getElementById("container");
+const slider = document.getElementById("sizeRange");
 
-(function() {
+(function () {
     console.log('IIFE invoked');
     defaultGrid();
-   
-   document.getElementById('reset-button').onclick = resetGrid;
+
+    document.getElementById('reset-button').onclick = resetGrid;
 })();
 
 // Skapar en grid 16x16
 
 function defaultGrid() {
-    console.log('defaultgrid function');    
+    console.log('defaultgrid function');
 
-    makeRows(8, 8);
+    makeRows(64, 64);
 
     let gridItems = [...document.getElementsByClassName('grid-item')];
-    console.log('griditems:', gridItems);
     gridItems.forEach(gridItem => {
         gridItem.addEventListener('mouseover', function (e) {
-            console.log('grid item hovered:', e.target);
             e.target.classList.add('mouseHover');
-            
+
         });
     });
 }
@@ -30,21 +29,26 @@ function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
-      let cell = document.createElement("div");
-      //cell.innerText = (c + 1);
-      container.appendChild(cell).className = "grid-item";
+        let cell = document.createElement("div");
+        //cell.innerText = (c + 1);
+        container.appendChild(cell).className = "grid-item";
     };
-  };
+};
 
-  //Reset grid - med knapp
-function resetGrid(){
-    console.log('resetGrid');
+//Reset grid - med knapp - samt kunna förändra storlek på grid utan att påverka totala antal pixlar. 
+function resetGrid() {
     let grid = document.querySelectorAll('.mouseHover');
     grid.forEach(mouseHover => {
         mouseHover.classList.remove('mouseHover');
     });
-
 }
 
+function setSize() {
+    
+}
+// Sätter storleken på Container till en fast storlek
+document.getElementById('container').style.height ="400px";
+document.getElementById('container').style.width ="600px";
+document.getElementById('container').style.borderRadius="200px";
 
 
